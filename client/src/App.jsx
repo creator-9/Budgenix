eaace6e2769076954bd10ee8ec38696130bb1cfd
Samprad <sampraaddas2503@gmail.com>
2025-11-14 17:02:13 +0530

    fixed imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Dashboard from "./pages/dashboard";
-import { Routes, Route } from "react-router";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
