c31441694920d24a989ddc33ae335c564125bd4c
kaihere14 <yashpalsharma9950@gmail.com>
2025-11-15 05:46:19 +0530

    fix: format code by adding space after commas in gemini.routes.ts

--- server/src/routes/gemini.routes.ts
+++ server/src/routes/gemini.routes.ts
@@ -3,6 +3,6 @@ import googleAi from "../controllers/gemini.controller.js";
 import { verifyJwt } from "../middleware/verifyJwt.js";
 const route = Router();
 
-route.post("/generateai",verifyJwt, googleAi);
+route.post("/generateai", verifyJwt, googleAi);
 
-export default route;   
+export default route;
